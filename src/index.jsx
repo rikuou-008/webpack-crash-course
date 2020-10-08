1d27c7337b9c398cb3f14b32272180968d9b528b
hashimotoeisuke <hashiz0314@icloud.com>
2020-10-08 15:14:36 +0900

    apply uglifyjs-webpack-plugin

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+console.log("I am priniting logs");
+
+ReactDOM.render (
+  <div>Hello, React!</div>,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin =  require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 
 const outputPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
-  entry: './src/index.js',
+  entry:  './src/index.jsx',
   output: {
     filename: 'main.js',
     path: outputPath
@@ -11,16 +15,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
-        test: /\.scss$/,
+        test: /\.(sc|c)ss$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader'
         ]
@@ -32,10 +39,32 @@ module.exports = {
          limit: 2048,
          name: './images/[name].[ext]'
        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
       }
     ]
   },
   devServer: {
     contentBase: outputPath
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css'
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions: {
+          compress: {
+            drop_console: true 
+          }
+      }
+    })],
   }
 }
